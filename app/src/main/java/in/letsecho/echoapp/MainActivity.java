c92da0715f29a5ddd8c301672360f1f605ad215c
tdublish3 <tdublish3@gatech.edu>
2017-01-06 03:40:15 +0530

    Added location service, still not working

--- app/src/main/java/in/letsecho/echoapp/MainActivity.java
+++ app/src/main/java/in/letsecho/echoapp/MainActivity.java
@@ -1,12 +1,18 @@
 package in.letsecho.echoapp;
 
+import android.*;
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -20,6 +26,9 @@ import com.firebase.jobdispatcher.Lifetime;
 import com.firebase.jobdispatcher.RetryStrategy;
 import com.firebase.jobdispatcher.Trigger;
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -33,12 +42,15 @@ import in.letsecho.library.UserProfile;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static String TAG = "MainActivity";
     public static final int RC_SIGN_IN = 1;
+    private static final int MY_PERMISSION_ACCESS_COURSE_LOCATION = 2;
 
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mUsersDbRef;
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseUser mCurrentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,21 +81,17 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
+            public void onTabUnselected(TabLayout.Tab tab) {}
 
             @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
+            public void onTabReselected(TabLayout.Tab tab) {}
         });
 
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
+                mCurrentUser = firebaseAuth.getCurrentUser();
+                if (mCurrentUser != null) {
                     // User is signed in
                 } else {
                     // User is signed out
@@ -99,7 +107,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
-
+        checkForPermissions();
         InitiateLocationSyncJob();
     }
 
@@ -110,15 +118,17 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 // Sign-in succeeded, set up the UI
                 // Saving user info in DB. To do: Should not happen on every sign in
-                FirebaseUser user = mFirebaseAuth.getCurrentUser();
-                UserProfile userProfile = new UserProfile(user.getUid(), user.getEmail(), user.getDisplayName(), user.getPhotoUrl().toString());
-                mUsersDbRef.child(user.getUid()).setValue(userProfile);
+                mCurrentUser = mFirebaseAuth.getCurrentUser();
+                UserProfile userProfile = new UserProfile(mCurrentUser.getUid(), mCurrentUser.getEmail(), mCurrentUser.getDisplayName(), mCurrentUser.getPhotoUrl().toString());
+                mUsersDbRef.child(mCurrentUser.getUid()).setValue(userProfile);
                 Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show();
 
                 //To insert Insatance id for existing users. Can be removed after 1st Feb 2017.
                 MyFirebaseInstanceIDService firebaseInstance = new MyFirebaseInstanceIDService();
                 String refreshedToken = FirebaseInstanceId.getInstance().getToken();
                 firebaseInstance.sendRegistrationToServer(refreshedToken);
+
+                InitiateLocationSyncJob();
             } else if (resultCode == RESULT_CANCELED) {
                 // Sign in was canceled by the user, finish the activity
                 Toast.makeText(this, "Sign in canceled", Toast.LENGTH_SHORT).show();
@@ -153,6 +163,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.sign_out_menu:
+                mCurrentUser = null;
                 AuthUI.getInstance().signOut(this);
                 return true;
             default:
@@ -161,26 +172,59 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void InitiateLocationSyncJob(){
-        // Create a new dispatcher using the Google Play driver.
-        FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(this));
-        dispatcher.cancel("location-update-job");
-        Job myJob = dispatcher.newJobBuilder()
-                // the JobService that will be called
-                .setService(LocationSyncService.class)
-                // uniquely identifies the job
-                .setTag("location-update-job")
-                // one-off job
-                .setRecurring(true)
-                // persist job forever
-                .setLifetime(Lifetime.FOREVER)
-                // start between 0 and 5*60 seconds from now
-                .setTrigger(Trigger.executionWindow(0, 5*60))
-                // don't overwrite an existing job with the same tag
-                .setReplaceCurrent(false)
-                // retry with exponential backoff
-                .setRetryStrategy(RetryStrategy.DEFAULT_EXPONENTIAL)
-                .build();
+        int locationPermission = ContextCompat.checkSelfPermission( this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if ( locationPermission == PackageManager.PERMISSION_GRANTED ) {
+            // Create a new dispatcher using the Google Play driver.
+            FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(this));
+            dispatcher.cancel("location-update-job");
+            Bundle userBundle = new Bundle();
+            //userBundle.putString("userId", mCurrentUser.getUid());
+            Job myJob = dispatcher.newJobBuilder()
+                    // the JobService that will be called
+                    .setService(LocationSyncService.class)
+                    // uniquely identifies the job
+                    .setTag("location-update-job")
+                    // one-off job
+                    .setRecurring(true)
+                    // persist job forever
+                    .setLifetime(Lifetime.FOREVER)
+                    // start between 0 and 5*60 seconds from now
+                    .setTrigger(Trigger.executionWindow(0, 60))
+                    // don't overwrite an existing job with the same tag
+                    .setReplaceCurrent(false)
+                    // retry with exponential backoff
+                    .setRetryStrategy(RetryStrategy.DEFAULT_EXPONENTIAL)
+                    .setExtras(userBundle)
+                    .build();
 
-        dispatcher.mustSchedule(myJob);
+            dispatcher.mustSchedule(myJob);
+        }
+    }
+
+    private void checkForPermissions() {
+        int locationPermission = ContextCompat.checkSelfPermission( this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if ( locationPermission != PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat.requestPermissions( this, new String[] {  Manifest.permission.ACCESS_FINE_LOCATION  },
+                    MY_PERMISSION_ACCESS_COURSE_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSION_ACCESS_COURSE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    InitiateLocationSyncJob();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
--- app/src/main/java/in/letsecho/echoapp/service/LocationSyncService.java
+++ app/src/main/java/in/letsecho/echoapp/service/LocationSyncService.java
@@ -1,22 +1,130 @@
 package in.letsecho.echoapp.service;
 
 import android.app.Service;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.firebase.jobdispatcher.JobParameters;
 import com.firebase.jobdispatcher.JobService;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
-public class LocationSyncService extends JobService {
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import in.letsecho.echoapp.R;
+
+public class LocationSyncService extends JobService implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
     private static String TAG = "LocationSyncService";
+    public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 5*1000;
+    public static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = UPDATE_INTERVAL_IN_MILLISECONDS / 2;
+    public static Boolean mRequestingLocationUpdates = false;
+
+    protected String mLastUpdateTime;
+    protected GoogleApiClient mGoogleApiClient;
+    protected LocationRequest mLocationRequest;
+    protected Location mCurrentLocation;
+
     @Override
     public boolean onStartJob(JobParameters job) {
         // Do some work here
+        Bundle userBundle = job.getExtras();
+//        String userId = userBundle.get("userId").toString();
+
+        buildGoogleApiClient();
+        createLocationRequest();
+        if (mGoogleApiClient.isConnected()) {
+            startLocationUpdates();
+        }
+
         Log.i(TAG, "Job is running");
         return false; // Answers the question: "Is there still work going on?"
     }
 
     @Override
     public boolean onStopJob(JobParameters job) {
+        stopLocationUpdates();
         return false; // Answers the question: "Should this job be retried?"
     }
+
+    protected void createLocationRequest() {
+        mGoogleApiClient.connect();
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);  //In Milliseconds
+//        mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    /**
+     * Requests location updates from the FusedLocationApi.
+     */
+    protected void startLocationUpdates() {
+        if (!mRequestingLocationUpdates) {
+            mRequestingLocationUpdates = true;
+            int locationPermission = ContextCompat.checkSelfPermission( this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION );
+            if (locationPermission == PackageManager.PERMISSION_GRANTED) {
+                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+                Log.i(TAG, " startLocationUpdates===");
+            }
+        }
+    }
+
+    /**
+     * Removes location updates from the FusedLocationApi.
+     */
+    protected void stopLocationUpdates() {
+        if (mRequestingLocationUpdates) {
+            mRequestingLocationUpdates = false;
+            Log.d(TAG, "stopLocationUpdates();==");
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+        Log.d(TAG, "Latitude:==" + mCurrentLocation.getLatitude() + "\n Longitude:==" + mCurrentLocation.getLongitude());
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        Log.i(TAG, "Building GoogleApiClient===");
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        startLocationUpdates();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        // The connection to Google Play services was lost for some reason. We call connect() to
+        // attempt to re-establish the connection.
+        Log.i(TAG, "Connection suspended==");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
+        // onConnectionFailed.
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + connectionResult.getErrorCode());
+    }
 }
+
